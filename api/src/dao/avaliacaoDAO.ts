46cc78f14c3db813144a4634d785a767792823f5
jc00973 <fonseca.jc@live.com>
2019-11-25 02:33:42 -0300

    Definição arquivos dao.

---
+++ api/src/dao/avaliacaoDAO.ts
@@ -0,0 +1,38 @@
+import Sequelize from 'sequelize';
+
+const Model = Sequelize.Model;
+
+class AvaliacaoDAO extends Model {}
+AvaliacaoDAO.init({
+    // attributes
+    notaDidatica: {
+        type: Sequelize.DOUBLE
+    },
+    notaCoerencia: {
+        type: Sequelize.DOUBLE
+    },
+    notaPontualidade: {
+        type: Sequelize.DOUBLE
+    },
+    notaCordialidade: {
+        type: Sequelize.DOUBLE
+    },
+    cursariaNovamente: {
+        type: Sequelize.BOOLEAN
+    },
+    mediaNotas: {
+        type: Sequelize.DOUBLE
+    },
+    comentario: {
+        type: Sequelize.STRING
+    },
+    tituloComentario: {
+        type: Sequelize.STRING
+    },
+}, {
+    sequelize,
+    modelName: 'avaliacaoDAO'
+    // options
+});
+
+exports default AvaliacaoDAO;
