8d09d08c73b27b6b33f88fd5af4f8d1e7771cc8c
José da Costa <josecxsta@gmail.com>
2019-11-25 15:25:33 -0300

    get avaliacoes

--- api/src/lib/avaliacao.ts
+++ api/src/lib/avaliacao.ts
@@ -1,16 +1,25 @@
-import database from "../config/database";
+import AvaliacaoDao from "../dao/avaliacao-dao";
 
 /**
- * @class UnidadeAcademica
+ * @class Avaliacao
  * @author José da Costa <josecxsta@gmail.com>
  */
-export default class UnidadeAcademica {
+export default class Avaliacao {
     id: number;
     nome: string;
     dataCriacao: Date;
     dataAtualizacao: Date;
 
     constructor() {
+    }
+
+    static async getAll(docente) {
+        let avaliacoes;
+        await AvaliacaoDao.findOne({ where: {docente: docente} })
+        .then(result => {
+            avaliacoes = result;
+        });
+        return avaliacoes;
     }
 
 }
--- api/src/routes/avaliacoes.route.ts
+++ api/src/routes/avaliacoes.route.ts
@@ -18,8 +18,15 @@ avaliacaoRoute.param("id", function (req: any, res, next, id) {
 
 
 avaliacaoRoute.route("/:id")
-.get(async function (req: any, res) {
-    res.send({});
+.get(
+    /**
+     * Obtém todas as avaliações de um docente.
+     * @param req
+     * @param res
+     */
+    async function (req: any, res) {
+    const avaliacoes = await Avaliacao.getAll(req.discente.id);
+    res.send(avaliacoes);
 });
 
 export default avaliacaoRoute;
