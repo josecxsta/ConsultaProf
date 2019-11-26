a6c35f10f3fcaa353d09618800e9c1c879fdad5f
José da Costa <josecxsta@gmail.com>
2019-11-25 23:53:34 -0300

    teste do endpoint de avaliações

---
+++ api/test/avaliacoes.test.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+import request from "supertest";
+import app from "../src/app";
+
+describe("Avaliações do Docente 1", () => {
+
+    describe("GET /avaliacoes/1", () => {
+        it("deve retornar 200", () => {
+            return request(app)
+                .get("/avaliacoes/1")
+                .expect(200);
+        });
+    });
+
+});
